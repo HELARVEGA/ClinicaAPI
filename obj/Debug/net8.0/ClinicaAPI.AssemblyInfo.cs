f47b30369084d24ffda96b3efde51a8b4da53dfa
HELARVEGA <022100603d@uandina.edu.pe>
2026-05-12 16:47:30 -0500

    Implementacion de pruebas unitarias xUnit

--- obj/Debug/net8.0/ClinicaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinicaAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+014cba8bb43d59cc3079484240518ef1cfaafe93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bdd7dcd06f6efac03096fd8a3241b852f31e26d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
